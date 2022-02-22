9e9975c3ada28565168b4ea4d70edcdb82739558
zhu <443020108@qq.com>
2022-02-22 22:24:19 +0800

    提交销售流程model

---
+++ DataBaseModels/sale_order_model.py
@@ -0,0 +1,16 @@
+
+class SaleOrderFormModel:
+    """
+    表单数据对象
+    """
+    def __init__(self):
+        self.m_szOrderID = None
+        self.m_szSubmitSaleName = None
+        self.m_szOptionalName = None
+
+        # 销售流程创建时间
+        self.m_nCreateTime = None
+
+
+
+
